f38a9623e51288d5d0bb706a168ddde0571857af
Paul Badour <snapson111@gmail.com>
2019-09-07 11:11:01 -0400

    Started Court Lines

--- Tennis Sim/src/main/Game.java
+++ Tennis Sim/src/main/Game.java
@@ -1,17 +1,47 @@
 package main;
 
+import java.awt.Color;
+import java.awt.FlowLayout;
+
 import javax.swing.JFrame;
+import javax.swing.JPanel;
 
 public class Game {
 	
 	private JFrame window = new JFrame("Tennis Simulator");
 	
-	private void setupCourt() {
-		window.setSize(800, 600);
+	private void setupCourt() { //                             78 / 36
+		
+		JPanel background = new JPanel();
+		JPanel courtGround = new JPanel();
+		JPanel net = new JPanel();
+		
+		// Window Setup
+		window.setSize(1100, 700);
 		window.setLayout(null);
 		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		window.setLocationRelativeTo(null);
 		window.setVisible(true);
+		
+		// Background
+		background.setBackground(Color.DARK_GRAY);
+		background.setBounds(0, 0, 1100, 700);
+		
+		// Green Court Ground
+		courtGround.setLayout(new FlowLayout());
+		courtGround.setBackground(new Color(3, 154, 3));
+		courtGround.setBounds(150, 150, 780, 360);
+		
+		
+		// Net
+		net.setLayout(new FlowLayout());
+		net.setBackground(Color.LIGHT_GRAY);
+		net.setBounds(535, 150, 5, 360);
+		
+		
+		window.add(net);
+		window.add(courtGround);
+		window.add(background);
 	}
 	
 	
